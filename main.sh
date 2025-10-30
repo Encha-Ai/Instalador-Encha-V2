db68828808a38671e0beb59c3816ac4463cf4482
Encha.ai <atendimento@encha.ai>
2025-10-30 11:37:44 -0300

    Update main.sh

--- main.sh
+++ main.sh
@@ -323,7 +323,7 @@ fi
 
 # Baixar novo script
 status_info "Baixando o Instalador Encha da fonte oficial..."
-if curl -fsSL --retry 3 --connect-timeout 10 https://raw.githubusercontent.com/Encha-Ai/Instalador-Encha/main/secondary.sh -o SetupEnchaAI; then
+if curl -fsSL --retry 3 --connect-timeout 10 https://github.com/Encha-Ai/Instalador-Encha/releases/download/instalador/SetupEnchaAI.sh -o SetupEnchaAI; then
     chmod +x SetupEnchaAI
     status_ok "Instalador Encha baixado com sucesso"
     
@@ -372,3 +372,4 @@ echo -e "${azul}🌐 Website: https://encha.ai${reset}"
 echo -e "${azul}📱 Instagram: @encha_ai${reset}"
 echo -e "${azul}📱 WhatsApp (suporte): +55 61 99159-2205${reset}"
 echo ""
+
